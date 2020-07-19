ef05bc0d79c52be93f6890936ae9bbd5abd79fec
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-07-19 22:15:37 +0200

    UserLocationManager

---
+++ NearBy/User Location Manager/UserLocationManager.swift
@@ -0,0 +1,94 @@
+//
+//  UserLocationManager.swift
+//  NearBy
+//
+//  Created by Ahmed Moussa on 7/19/20.
+//  Copyright © 2020 Moussa Tech. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol UserLocationManagerDelegate: class {
+    func userLocationUpdate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+}
+
+class UserLocationManager: NSObject {
+    let locationManager = CLLocationManager()
+    var delegate: UserLocationManagerDelegate? = nil
+    
+    override init() {
+        super.init()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = LocationAccuracy.nearestTenMeters.value()
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    private enum LocationAccuracy {
+        case bestForNavigation
+        case best
+        case hundredMeters
+        case kilometer
+        case threeKilometers
+        case nearestTenMeters
+        
+        func value() -> CLLocationAccuracy {
+            switch self {
+            case .bestForNavigation:
+                return kCLLocationAccuracyBestForNavigation
+            case .best:
+                return kCLLocationAccuracyBest
+            case .hundredMeters:
+                return kCLLocationAccuracyHundredMeters
+            case .kilometer:
+                return kCLLocationAccuracyKilometer
+            case .threeKilometers:
+                return kCLLocationAccuracyThreeKilometers
+            case .nearestTenMeters:
+                return kCLLocationAccuracyNearestTenMeters
+            }
+        }
+    }
+}
+
+extension UserLocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        self.delegate?.userLocationUpdate(latitude: locValue.latitude, longitude: locValue.longitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways:
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = LocationAccuracy.nearestTenMeters.value()
+            self.locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = LocationAccuracy.nearestTenMeters.value()
+            self.locationManager.startUpdatingLocation()
+        case .denied:
+            break
+        case .notDetermined:
+            break
+        case .restricted:
+            break
+        @unknown default:
+            print("not handled CL Authorization Status \(status)")
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?) {
+        print(error ?? nil)
+    }
+}
