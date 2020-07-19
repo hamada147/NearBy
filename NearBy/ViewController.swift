5b924ee76faea008119fa66c9829722f46ae3eda
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-07-19 13:49:55 +0200

    Initial Commit

---
+++ NearBy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearBy
+//
+//  Created by Ahmed Moussa on 7/19/20.
+//  Copyright © 2020 Moussa Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
